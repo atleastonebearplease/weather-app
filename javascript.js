1a03ac754e2729a92170d2a2081815caddb971c1
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-01-13 07:41:20 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/* https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/huntingdon%2C%20tennessee?
+  unitGroup=us
+  &include=days,hours,current
+  &key=8ZEGCZEHNLG7BUTQE56KUANQL
+  &contentType=json 
+  
+    https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/
+    timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY
+
+  */
+//Call to the API for the data that you need
+
+//My KEY: 8ZEGCZEHNLG7BUTQE56KUANQL
